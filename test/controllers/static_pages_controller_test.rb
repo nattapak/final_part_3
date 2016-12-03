8b2819918652cbba45a49e849f1a59b3e31edd61
nattapak <nattapak001@hotmail.com>
2016-12-03 15:57:55 +0700

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,18 @@
-require 'test_helper'
-
+ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-  end
-
-  test "should get help" do
-    get static_pages_help_url
-    assert_response :success
-  end
-
-end
+ test "should get home" do
+ get static_pages_home_url
+ assert_response :success
+ assert_select "title", "เป็นเว็บรวมบทเพลงพระราชนิพนธ์"
+ end
+ test "should get help" do
+ get static_pages_help_url
+ assert_response :success
+ assert_select "title", "ไกลกังวล"
+ end
+ test "should get about" do
+ get static_pages_about_url
+ assert_response :success
+ assert_select "title", "นกน้อยคอยบิน"
+ end
+end 
